3718f89684a9766327a532383f394126be2a6aae
Gabriel Miller <gabrielmillerone@hotmail.com>
2021-09-15 00:16:48 -0300

    Starting GitHub Explorer with ReactJS

---
+++ dist/bundle.js
@@ -0,0 +1,8 @@
+"use strict";
+
+var _user$address;
+
+var user = {
+  name: 'Gabriel'
+};
+console.log((_user$address = user.address) === null || _user$address === void 0 ? void 0 : _user$address.street);
---
+++ src/index.js
@@ -0,0 +1,5 @@
+const user = {
+    name: 'Gabriel',
+}
+
+console.log(user.address?.street)
