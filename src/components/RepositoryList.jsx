2f24d36bdf9f3329ce33e9e05ec6cd9f9c72318c
Gabriel Miller <gabrielmillerone@hotmail.com>
2021-09-17 01:12:53 -0300

    Fetching the github API

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,6 +1,9 @@
 import { RepositoryItem } from "./RepositoryItem";
 
-import '../styles/repositories.scss'
+import '../styles/repositories.scss';
+import { useState, useEffect } from "react";
+
+//https://api.github.com/orgs/rocketseat/repos
 
 const repository = {
         name: "unform2",
@@ -9,6 +12,15 @@ const repository = {
     };
 
 export function RepositoryList() {
+
+    const [repositories, setRepositories] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.github.com/orgs/rocketseat/repos')
+        .then(response => response.json())
+        .then(data => setRepositories(data))
+    }, []);
+
     return (
         <section className="repository-list">
             <h1>Lista de Repositórios</h1>
